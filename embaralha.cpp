5ef00f49d590f435f6202b5cd349ffb8da83d364
MatheusLadeiras <69425532+MatheusLadeiras@users.noreply.github.com>
2022-01-26 23:11:35 -0300

    Add files via upload

---
+++ embaralha.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <clocale>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+
+const int max_tamanho = 101;
+
+void embaralha (char arr[], int size) {
+	srand(time(0)); 
+	
+    for (int i = 0; i < size; i++) {
+        int j = rand() % size;
+        char t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    //printf("j: %i \n", j);
+    //printf("i: %i \n", i);
+    //printf("t: %c \n", t);
+    }
+    
+}
+
+int verifica_palavra (char arr[], int size) {
+	//printf("size: %i \n", size);
+	int i = 0;
+	if (size < 5 || size > 50) {
+		return 0;
+	} else {
+		do {
+			if(!isalpha(arr[i])) {
+				return 0; 
+			}
+			i++;
+		} while(arr[i] != '\0');
+	}	
+	return 1;//correto
+}
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    char palavra[max_tamanho];
+    
+    do {
+    	int i = 0;
+    	int tamanho = 0;
+    	
+    	memset(palavra,0,strlen(palavra));
+    	
+	    printf("Digite a palavra a ser embaralhada: ");
+	    gets(palavra);
+	    
+	    if(palavra[0] != '\0') {
+		    while(palavra[i] != '\0') {
+		    	i++;
+		    }
+		    tamanho = i;
+		    //printf("tamanho: %i \n", tamanho);
+		    
+			int m = verifica_palavra(palavra, tamanho);
+	    	printf("O verifica_palavra retornou %i\n", m);
+	    	
+	    	if (m == 1) {
+	    		printf("A palavra %s se tornou", palavra);
+			    embaralha(palavra, tamanho);
+			    printf(" %s. \n", palavra);	
+			} else if (m == 0) {
+				printf("Não é uma palavra. \n");
+			}	    
+		} else {
+			printf("Fechando programa");
+		}
+		
+	} while (palavra[0] != '\0');
+
+    return 0;
+}
+
+
